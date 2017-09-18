e7cbaceb58669113cbc3b017fbab2a44cccc4b15
Chen Zhiwei <zhiweik@gmail.com>
2017-09-18 09:11:16 +0000

    add symlink for nginx dir

--- nginx-multiarch/nginx.sh
+++ nginx-multiarch/nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 index_html=/var/lib/nginx/html/index.html
-if grep -q 'Welcome to nginx' $index_html &>/dev/null; then
-    echo "Hello Nginx version ${NGINX_VERSION}, hostname $(hostname)" > $index_html
+if ! [[ -e $index_html ]] || grep -q 'Welcome to nginx' $index_html &>/dev/null; then
+    echo "Hello Nginx version ${NGINX_VERSION}, arch $(uname -m) and hostname $(hostname)" > $index_html
 fi
 
 if [[ "$1" == "" ]]; then
