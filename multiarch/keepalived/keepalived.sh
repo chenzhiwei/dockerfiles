919a8564b7eccf12e62e3a0e9acbedcf8855f343
Chen Zhiwei <zhiweik@gmail.com>
2017-10-21 13:33:11 +0000

    catch sigterm signal

--- multiarch/keepalived/keepalived.sh
+++ multiarch/keepalived/keepalived.sh
@@ -2,13 +2,20 @@
 
 AUTO_CONFIGURE=${AUTO_CONFIGURE:-true}
 
+function sigterm_handler() {
+    local pids=$(ps -ef | awk '/ keepalived / {print $1}')
+    kill -SIGTERM $pids
+    wait $pids
+    exit 143
+}
+
 function create_config() {
     if [[ "$INTERFACE" == "" ]] || [[ "$VIP" == "" ]]; then
         echo '"INTERFACE" and "VIP" environment variables must be provided'
         exit 1
     fi
 
-    AUTH_PASS=${AUTH_PASS:-1111}
+    AUTH_PASS=${AUTH_PASS:-$(echo $VIP | md5sum | awk '{print $1}')}
     VRID=$(echo $VIP | awk -F'[./]' '{print $4}')
 
     sed -i -e 's/HOSTNAME/'$HOSTNAME'/g' -e 's/INTERFACE/'$INTERFACE'/g' -e 's/VRID/'$VRID'/g' -e 's/AUTH_PASS/'$AUTH_PASS'/g' -e 's#VIP#'$VIP'#g' /etc/keepalived/keepalived.conf
@@ -18,4 +25,6 @@ if [[ "$AUTO_CONFIGURE" == "true" ]]; then
     create_config
 fi
 
-keepalived -n -l
+trap 'kill ${!}; sigterm_handler' SIGTERM
+
+keepalived -n -l & wait ${!}
