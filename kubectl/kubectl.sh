e0a6d5e47a213edf1bbb684e26df6e88d069fcc0
Chen Zhiwei <zhiweik@gmail.com>
2019-05-21 19:53:43 +0800

    update kubectl dockerfile

--- kubectl/kubectl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-kubernetes_service_url="https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT_HTTPS"
-
-kubectl config set-credentials user --token=$token
-kubectl config set-cluster kube-cluster --server=$kubernetes_service_url --insecure-skip-tls-verify=true
-
-${@}
