4c5394f454365d6a66acc313daf31b8331e19270
Chen Zhiwei <zhiweik@gmail.com>
2021-08-22 14:07:05 +0800

    update hello server

--- archive/hello-world/server.go
+++ archive/hello-world/server.go
@@ -1,17 +1,22 @@
 package main
+
 import (
 	"fmt"
-	"net/http"
 	"io"
+	"net/http"
+	"os"
+	"time"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!\n")
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	host, _ := os.Hostname()
+	resp := fmt.Sprintf("time=%v, host=%s, uri=%s\n", time.Now().UTC(), host, r.RequestURI)
+	fmt.Println(resp)
+	io.WriteString(w, resp)
 }
 
-
 func main() {
-	fmt.Println("HTTP Server starts at port 80")
-	http.HandleFunc("/", helloHandler)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", rootHandler)
+	http.ListenAndServe(":8080", mux)
 }
