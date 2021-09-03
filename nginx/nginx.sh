e81707ea8a0a43f51813536ec577ea2ac9bef31b
Chen Zhiwei <zhiweik@gmail.com>
2021-09-03 18:13:43 +0800

    update nginx

--- nginx/nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-index_html=/var/lib/nginx/html/index.html
-if ! [[ -e $index_html ]] || grep -q 'Welcome to nginx' $index_html &>/dev/null; then
-    echo "Hello Nginx version ${NGINX_VERSION}, arch $(uname -m) and hostname $(hostname)" > $index_html
-fi
-
-nginx -g "daemon off;"
