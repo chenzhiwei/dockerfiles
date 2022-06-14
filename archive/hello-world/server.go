c3079bdcaa21cc31f03f88624878642660a875a9
Chen Zhiwei <zhiweik@gmail.com>
2022-06-14 16:26:06 +0800

    update server.go

--- archive/hello-world/server.go
+++ archive/hello-world/server.go
@@ -10,13 +10,18 @@ import (
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	host, _ := os.Hostname()
-	resp := fmt.Sprintf("time=%v, host=%s, uri=%s\n", time.Now().UTC(), host, r.RequestURI)
+	resp := fmt.Sprintf("time=%v, host=%s, method=%s, uri=%s\n", time.Now().Format("20060102T150405Z0700"), host, r.Method, r.RequestURI)
 	fmt.Println(resp)
 	io.WriteString(w, resp)
 }
 
 func main() {
+	addr := os.Getenv("ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(addr, mux)
 }
