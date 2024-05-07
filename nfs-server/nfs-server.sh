a8f4109f029475f7170e9d1a6749c43bd7b54e98
Chen Zhiwei <zhiwei@youya.org>
2024-05-07 14:48:16 +0800

    Update nfs-server.sh

--- nfs-server/nfs-server.sh
+++ nfs-server/nfs-server.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 
-function start() {
+function _start() {
     rpcbind -w
     mount -t nfsd nfsd /proc/fs/nfsd
     rpc.mountd
     exportfs -r
-    rpc.nfsd -G 10
-    rpc.statd --no-notify
-
-    showmount -e
-    if [[ $? -eq 0 ]]; then
-        echo "NFS Server started"
-    else
-        exit 1
-    fi
+    rpc.nfsd -G 5
+    rpc.statd --no-notify -F &
+    wait ${!}
 }
 
-function stop() {
+function _stop() {
     rpc.nfsd 0
     exportfs -au
     exportfs -f
-    kill $(pidof rpc.mountd)
+    kill $(pidof rpc.statd) $(pidof rpc.mountd) $(pidof rpcbind)
     umount /proc/fs/nfsd
 
     exit 0
 }
 
-trap stop TERM
+trap _stop SIGTERM
 
-start
-
-while true; do
-    sleep 99999d
-done
+_start
