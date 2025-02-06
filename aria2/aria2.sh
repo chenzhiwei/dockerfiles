6c68e82db6299e0b83f799afbdb386b1c842d368
Chen Zhiwei <zhiwei@youya.org>
2025-02-06 13:56:23 +0800

    update aria2

--- aria2/aria2.sh
+++ aria2/aria2.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 ARIA2_DIR=${ARIA2_DIR:-/downloads}
-ARIA2_DISK_CACHE=${ARIA2_DISK_CACHE:-256M}
+ARIA2_DISK_CACHE=${ARIA2_DISK_CACHE:-128M}
 ARIA2_RPC_SECRET=${ARIA2_RPC_SECRET:-aria2c}
+ARIA2_SPLIT=${ARIA2_SPLIT:-5}
+ARIA2_MAX_CONNECTION_PER_SERVER=${ARIA2_MAX_CONNECTION_PER_SERVER:-1}
 
 mkdir -p /config
 sed -e "s@dir=.*@dir=${ARIA2_DIR}@g" \
     -e "s@disk-cache=.*@disk-cache=${ARIA2_DISK_CACHE}@g" \
     -e "s@rpc-secret=.*@rpc-secret=${ARIA2_RPC_SECRET}@g" \
+    -e "s@split=.*@split=${ARIA2_SPLIT}@g" \
+    -e "s@max-connection-per-server=.*@max-connection-per-server=${ARIA2_MAX_CONNECTION_PER_SERVER}@g" \
     /aria2.conf > /config/aria2.conf
 
 touch /config/aria2.session
